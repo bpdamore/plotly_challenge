46190000093a2e143e721d036b49897705bf1d64
bdamore916 <bdamore916@gmail.com>
2020-07-12 17:12:27 -0500

    graphs are ready

--- static/js/app.js
+++ static/js/app.js
@@ -8,12 +8,23 @@
 // Use sample_values as the values for the bar chart.
 // Use otu_ids as the labels for the bar chart.
 // Use otu_labels as the hovertext for the chart.
+// Add p in div with id sample-metadata
 
-// for(var i=1; i<=24; i++){
-//   d3.selectAll("#selDataset").
-// }
+function init() {
+  Plots(0)
+}
 
-// d3.selectAll("#selDataset").on("change", barPlot)
+for(var i=0; i<=152; i++){
+  d3.selectAll("#selDataset")
+  .append("option").text(i)
+}
+
+d3.selectAll("#selDataset")
+  .on("change", (console.log(this)))
+
+
+
+// d3.selectAll("#selDataset").on("change", Plots(sampleID))
 
 function Plots(sampleID) {
 
@@ -23,29 +34,46 @@ function Plots(sampleID) {
   let url = "data/samples.json"
   let sampSize = 10
   d3.json(url).then(function(data){
+    // Define variables for our graphs
     let sampleInfo = data
     let sample = +data.samples[sampleID].id
-    let otuIDs = (data.samples[sampleID].otu_ids).slice(0,sampSize)
-    let otuLabels = (data.samples[sampleID].otu_labels).slice(0,sampSize)
-    let otuValues = (data.samples[sampleID].sample_values).slice(0,sampSize)
-    console.log(sample)
-    console.log(otuIDs)
-    console.log(otuValues)
-    console.log(otuLabels)
-    console.log(sampleInfo)
+    let otuIDs = (data.samples[sampleID].otu_ids)
+    let topOtuIDs = (data.samples[sampleID].otu_ids).slice(0,sampSize)
+    let otuLabels = (data.samples[sampleID].otu_labels)
+    let otuValues = (data.samples[sampleID].sample_values)
+    let topOtuLabels = (data.samples[sampleID].otu_labels).slice(0,sampSize)
+    let topOtuValues = (data.samples[sampleID].sample_values).slice(0,sampSize)
+    // Define metadata for the Demographic Info
+    let metaID = data.metadata[sampleID].id
+    let metaBb = data.metadata[sampleID].bbtype
+    let metaAge = data.metadata[sampleID].age
+    let metaEth = data.metadata[sampleID].ethnicity
+    let metaGen = data.metadata[sampleID].gender
+    let metaLoc = data.metadata[sampleID].location
+    let metaWFreq = data.metadata[sampleID].wfreq
+    console.log(data)
 
     let IDs = []
-    otuIDs.forEach(function(entry) {
+    topOtuIDs.forEach(function(entry) {
       IDs.push("OTU"+entry)
     })
     console.log(IDs)
+
+    d3.selectAll("#sample-metadata")
+      .append("p").text(`id: ${metaID}`)
+      .append("p").text(`ethnicity: ${metaEth}`)
+      .append("p").text(`gender: ${metaGen}`)
+      .append("p").text(`age: ${metaAge}`)
+      .append("p").text(`location: ${metaLoc}`)
+      .append("p").text(`bbtype: ${metaBb}`)
+      .append("p").text(`wfreq: ${metaWFreq}`)
 
 // Bar chart
     let trace1 = {
       type: "bar",
       y: IDs,
-      x: otuValues,
-      text: otuLabels,
+      x: topOtuValues,
+      text: topOtuLabels,
       orientation: "h"
     };
 
@@ -60,9 +88,14 @@ function Plots(sampleID) {
 
 // Bubble chart
     let trace2 = {
-      x: IDs,
+      x: otuIDs,
       y: otuValues,
-      mode: "markers"
+      text:otuLabels,
+      mode: "markers",
+      marker: {
+        size: otuValues,
+        color: otuIDs
+      }
     };
     let bubbleData = [trace2];
     let bubbleLayout = {
@@ -72,4 +105,4 @@ function Plots(sampleID) {
   })
 }
 
-Plots(1)
+init()
